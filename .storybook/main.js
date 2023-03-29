0f3f26cba1d451e3734ad7454105df4137b2956f
Jhon Arguello <jhon.arguello@icarotech.com>
2023-03-29 16:33:30 -0300

    UPT: COnfigurações iniciais.

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+  typescript: {
+    check: false, // type-check stories during Storybook build
+  },
+  webpackFinal: async (config, {configType}) => {
+    const tsConfigIndex = config.plugins.findIndex(v => v.constructor.name === "ForkTsCheckerWebpackPlugin")
+    config.plugins.splice(tsConfigIndex, 1); 
+    return config; 
+  }
+};
